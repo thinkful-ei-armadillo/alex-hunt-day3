956cd20f3cf8285499f85d6b5987f5072adfa71d
AReach <DerAReich@gmail.com>
2019-01-16 10:29:38 -0600

    added the 'personMaker' function

--- objdrills.js
+++ objdrills.js
@@ -1,5 +1,9 @@
 'use strict';
 
+//----------------------------//
+// Object Drills 1:           //
+//----------------------------//
+
 function createMyObject() {
     return {
         foo: 'bar',
@@ -7,4 +11,13 @@ function createMyObject() {
         'olly olly': 'oxen free',
         sayHello: function() { return 'hello' },
     }
+}
+
+function personMaker() {
+    var person = {
+        firstName: 'Paul',
+        lastName: 'Jones',
+        fullName: function() { return `${this.firstName} ${this.lastName}` },
+    };
+    return person;
 }
