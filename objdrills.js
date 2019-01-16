85484adce001b60595343946552064c9efe3fe13
AReach <DerAReich@gmail.com>
2019-01-16 10:55:57 -0600

    added the 'findById' function

--- objdrills.js
+++ objdrills.js
@@ -29,3 +29,11 @@ function personMaker() {
 function makeStudentReport(data) {
     return data.map(obj => `${obj.name}: ${obj.grade}`);
 }
+
+function findById(items, idNum) {
+    for (let i = 0; i < items.length; i++) {
+        if (items[i].id === idNum) {
+            return items[i];
+        }
+    }
+}
