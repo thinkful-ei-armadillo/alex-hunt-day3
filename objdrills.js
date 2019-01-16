16b2831a7aaaf62a48d8e85af220ec7cd891cb87
AReach <DerAReich@gmail.com>
2019-01-16 10:11:02 -0600

    added factory function 'createMyObject'

---
+++ objdrills.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function createMyObject() {
+    return {
+        foo: 'bar',
+        answerToUniverse: 42,
+        'olly olly': 'oxen free',
+        sayHello: function() { return 'hello' },
+    }
+}
