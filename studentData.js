24ca17cd18966cf746e988f1ae4f24f9ca68f2ba
Hunter Kreshock <hkreshock@gmail.com>
2019-01-16 09:44:57 -0700

    added studentData

---
+++ studentData.js
@@ -0,0 +1,24 @@
+const studentData = [
+  {
+    name: 'Tim',
+    status: 'Current student',
+    course: 'Biology',
+  },
+  {
+    name: 'Sue',
+    status: 'Withdrawn',
+    course: 'Mathematics',
+  },
+  {
+    name: 'Liz',
+    status: 'On leave',
+    course: 'Computer science',
+  },
+];
+
+function enrollInSummerSchool(students) {
+  for (i = 0; i < students.length; i++) {
+  students[i].status = 'In Summer School';
+  }
+ return students;
+  }
