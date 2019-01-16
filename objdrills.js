386c11ba0a2b0cbe592c6dce09409af50fe2904c
AReach <DerAReich@gmail.com>
2019-01-16 14:03:58 -0600

    added the array of objects function and the properties that aren't there test

--- objdrills.js
+++ objdrills.js
@@ -63,3 +63,21 @@ const objIterate = {
 // for (let key in objIterate) {
 //     console.log(key+": "+objIterate[key]);
 // }
+
+const employees = [
+    { name: 'Joe', jobTitle: 'Manager', boss: 'Rebecca' },
+    { name: 'Bob', jobTitle: 'Worker', boss: 'Joe' },
+    { name: 'Alex', jobTitle: 'King', boss: null },
+    { name: 'Rebecca', jobTitle: 'CEO', boss: 'Alex' },
+    { name: 'Emily', jobTitle: 'CFO', boss: 'Rebecca' }
+]
+
+//Test:
+// employees.forEach(obj => {
+//     console.log(`${obj.name} has the title of: ${obj.jobTitle}`);
+// });
+
+employees.forEach(obj => {
+    console.log(`${obj.jobTitle} ${obj.name} reports to ${obj.boss ? 'reports to '+obj.boss : "doesn't report to anybody"}.`);
+});
+
